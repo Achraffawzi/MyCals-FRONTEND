15fc9145b6a298305ec29a1f4f2e5e91b99062e6
achraffawzi <achraffawzi224a@gmail.com>
2021-08-11 13:47:29 +0200

    Adding the avatar in testimonial component

--- src/components/Home/TestimonialCard.vue
+++ src/components/Home/TestimonialCard.vue
@@ -8,22 +8,36 @@
         600: { items: 1, nav: false },
       }"
     >
-      <v-card outlined max-width="600" class="mx-auto testimonial__card" v-for="review in reviews" :key="review.id">
+      <v-card
+        outlined
+        max-width="600"
+        class="mx-auto testimonial__card"
+        v-for="review in reviews"
+        :key="review.id"
+      >
         <v-row wrap class="align-items-stretch">
           <v-col cols="12" sm="12" md="4">
-            <img
-              src="https://source.unsplash.com/random"
-              alt=""
-              class="testimonial__img"
-            />
-            <!-- <v-responsive>
-            </v-responsive> -->
+            <!-- current logged in User avatar -->
+            <v-avatar tile v-if="review.picture != null">
+              <img
+                style="width: 100%; height: 100%;"
+                tile
+                :src="review.picture"
+              />
+            </v-avatar>
+            <v-avatar style="width: 100%; height: 100%;" tile v-else color="primary">
+              <span class="white--text text-h3">{{ getUserNameAvatar }}</span>
+            </v-avatar>
           </v-col>
           <v-col cols="12" sm="12" md="8" class="quote-icon">
-            <v-card-title class="font-weight-bold"> {{ review.title }} </v-card-title>
+            <v-card-title class="font-weight-bold">
+              {{ review.title }}
+            </v-card-title>
             <v-card-text>
               <p>{{ review.text }}</p>
-              <h3 class="primary--text font-weight-bold">{{review.nameOfAuthor }}</h3>
+              <h3 class="primary--text font-weight-bold">
+                {{ review.nameOfAuthor }}
+              </h3>
               <v-rating
                 v-model="review.nbr_Stars"
                 color="primary"
@@ -47,6 +61,8 @@ export default {
   name: "TestimonialCard",
   data() {
     return {
+      userAvatarSrc: "",
+      getUserNameAvatar: "",
       rating: 4,
       reviews: [],
     };
@@ -63,8 +79,15 @@ export default {
     handleFetchReviews() {
       createApiEndPoints(END_POINTS.GET_REVIEWS)
         .fetch()
-        .then((response) => {;
+        .then((response) => {
           this.reviews = [...response.data];
+          this.reviews.forEach((review) => {
+            if (review.picture == null)
+              this.getUserNameAvatar =
+                review.nameOfAuthor.split(" ")[0].charAt(0).toUpperCase() +
+                "" +
+                review.nameOfAuthor.split(" ")[1].charAt(0).toUpperCase();
+          });
         })
         .catch((error) => console.log(error));
     },
@@ -89,5 +112,9 @@ export default {
 .testimonial__img {
   height: 240px;
   object-fit: contain;
+}
+
+@media screen and (max-width: 959px) {
+  .v-avatar { height: 200px !important }
 }
 </style>
